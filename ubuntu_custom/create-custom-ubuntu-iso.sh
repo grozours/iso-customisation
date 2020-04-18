0f1e27d1ade07ec5d7f93e0b47fceed2c655143d
grozours <7918202+grozours@users.noreply.github.com>
2020-04-19 01:41:32 +0200

    added some variables to script

--- ubuntu_custom/create-custom-ubuntu-iso.sh
+++ ubuntu_custom/create-custom-ubuntu-iso.sh
@@ -3,11 +3,15 @@
 iso="ubuntu-18.04.4-live-server-amd64.iso"
 origin="http://ftp.belnet.be/ubuntu-releases/18.04.4/"
 
+project="$HOME/iso-customisation/ubuntu_custom"
+
 # create directory structure et iso file
-[[ ! -d $HOME/livecd ]] && mkdir $HOME/livecd
-[[ ! -d $HOME/livecd/iso ]] && mkdir $HOME/livecd/iso
-[[ ! -d $HOME/livecd/squashfs ]] && mkdir $HOME/livecd/squashfs
-[[ ! -e $HOME/livecd/$iso ]] && wget $origin/$iso
+[[ ! -d $project ]] && mkdir -p $project
+[[ -d $project/work ]] && sudo rm -rf $project/work
+[[ ! -d $project/work ]] && mkdir $project/work
+[[ -d $project/squashfs ]] && sudo rm -rf $project/squashfs
+[[ ! -d $project/squashfs ]] && mkdir $project/squashfs
+[[ ! -e $project/iso/$iso ]] && wget $origin/$iso -o $project/iso/$iso
 
 [[ ! -d /tmp/iso ]] && mkdir /tmp/iso
 [[ ! -d /tmp/squashfs ]] && mkdir /tmp/squashfs
@@ -16,24 +20,24 @@ origin="http://ftp.belnet.be/ubuntu-releases/18.04.4/"
 sudo apt-get install squashfs-tools schroot genisoimage
 
 # extract from iso the content
-sudo mount -o loop $HOME/livecd/$iso /tmp/iso
-sudo cp -av /tmp/iso/. $HOME/livecd/iso
+sudo mount -o loop $project/iso/$iso /tmp/iso
+sudo cp -av /tmp/iso/. $project/work
 sudo umount /tmp/iso
 
 # extract squashfs
-sudo mount -t squashfs -o loop $HOME/livecd/iso/casper/filesystem.squashfs /tmp/squashfs
-sudo cp -av /tmp/squashfs/. $HOME/livecd/squashfs
+sudo mount -t squashfs -o loop $project/work/casper/filesystem.squashfs /tmp/squashfs
+sudo cp -av /tmp/squashfs/. $project/squashfs
 sudo umount /tmp/squashfs
 
-cd $HOME/livecd
+cd $project
 
 # mount need directory for chrooted system modifications
-#sudo mount --bind /proc squashfs/proc
-#sudo mount --bind /sys squashfs/sys
-#sudo mount -t devpts none squashfs/dev/pts
-#sudo mount --bind /dev squashfs/dev
-#sudo mount --bind /dev/pts squashfs/dev/pts
-#sudo mount --bind /var/run/dbus/ squashfs/var/run/dbus/
+sudo mount --bind /proc squashfs/proc
+sudo mount --bind /sys squashfs/sys
+sudo mount -t devpts none squashfs/dev/pts
+sudo mount --bind /dev squashfs/dev
+sudo mount --bind /dev/pts squashfs/dev/pts
+sudo mount --bind /var/run/dbus/ squashfs/var/run/dbus/
 
 # prepare network temporary access for chrooted system
 sudo cp /etc/resolv.conf squashfs/etc/resolv.conf
